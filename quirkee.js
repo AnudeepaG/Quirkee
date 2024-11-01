12574b7ef067823a297c6cda9d5957a47c353a2e
Anu <21022510@geu.ac.in>
2024-11-02 04:44:47 +0530

    Add files via upload

---
+++ quirkee.js
@@ -0,0 +1,48 @@
+// Array of background image URLs
+const backgroundImages = [
+    "images/pic1.jpg",
+    "images/pic2.jpg",
+    "images/pic3.jpg",
+    "images/pic4.jpg",
+    "images/pic5.jpg"
+];
+
+// Function to add background images
+function addBackgroundImages() {
+    const container = document.getElementById('background-images');
+    for (let i = 0; i < backgroundImages.length; i++) {
+        const img = document.createElement('img');
+        img.src = backgroundImages[i];
+
+        // Random position within the viewport
+        const randomX = Math.random() * 100; // Random percentage for left position
+        const randomY = Math.random() * 100; // Random percentage for top position
+        img.style.left = `${randomX}vw`;
+        img.style.top = `${randomY}vh`;
+
+        container.appendChild(img);
+    }
+}
+
+// Array of quirky quotes
+const quotes = [
+    "Be yourself; everyone else is already taken.",
+    "Life is too important to be taken seriously.",
+    "Normal is boring. Dare to be different!",
+    "Creativity is intelligence having fun.",
+    "Stay weird. It's better that way.",
+    "The world needs more sparkle and sass.",
+    "Embrace the glorious mess that you are.",
+    "Your vibe attracts your tribe.",
+    "Life is short, make it sweet.",
+    "Dare to be different; it's more fun that way!"
+];
+
+// Function to generate a new quote
+function generateQuote() {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    document.getElementById("quote").textContent = quotes[randomIndex];
+}
+
+// Call the function to add background images on page load
+window.onload = addBackgroundImages;
